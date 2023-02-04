9e84aaaf9e471ab41b42225e5562e5d07160ca8e
Nico Desktop <diaznicox96@gmail.com>
2023-02-04 15:56:49 -0300

    removed dynamicModal.js, refactoring code in app.js, add auxFunctions.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,13 @@
-import { createModalTickets, createElementCustom } from "./dynamicModal.js";
 import { validarEmail, validarNomApe } from "./validaciones.js";
-/* Formulario */
+import {addCustomPToModalError, textoValidarNomApe, textoValidarEmail, textoValidarNegativo, elegirCategoria, mostrarModalError, setValorFinalPorCategoria} from "./auxFunctions.js";
+
+/* Referencias a formularios error y de tickets */
 const $modalError = document.querySelector("#modal-error");
-const $contenedorModalTicket = document.querySelector("#modalTickets");
-const $modalTickets = createModalTickets();
-$contenedorModalTicket.appendChild($modalTickets);
+const $modalTicket = document.querySelector("#staticModalTicket");
 const $formTickets = document.forms[1];
 const $modalBodyError = document.querySelector("#modal-error-body");
-const $textNode1 = createElementCustom("p", [], "", {});
-const $textNode2 = createElementCustom("p", [], "", {});
-const $textNode3 = createElementCustom("p", [], "", {});
-const $textNode4 = createElementCustom("p", [], "", {});
-$modalBodyError.appendChild($textNode1);
-$modalBodyError.appendChild($textNode2);
-$modalBodyError.appendChild($textNode3);
-$modalBodyError.appendChild($textNode4);
-const newModal = new bootstrap.Modal("#modal-error", {keyboard: false});
+
+/* Referencia de elementos del formulario de Tickets */
 const $inputCantidad = document.querySelector("#form-cantidad");
 const $inputStaticValorFinal = document.querySelector("#valor-final");
 const $optionList = document.querySelector("#form-categorias");
@@ -23,108 +15,57 @@ const $inputNombre = document.querySelector("#form-nombre");
 const $inputApellido = document.querySelector("#form-apellido");
 const $inputEmail = document.querySelector("#form-email");
 
-$modalTickets.addEventListener("click", (e) => {
-    if (e.target.matches("#form-btn-resumen")){
-        e.preventDefault();
-        if (validarNomApe($inputNombre) == -1 || validarNomApe($inputApellido) == -1){
-            $textNode1.textContent = "Los campos nombre y apellido deben tener:";
-            $textNode2.textContent = "- Mínimo 3 caracteres y máximo 24.";
-            $textNode3.textContent = "- Puede contener acentos y mayúsculas.";
-            $textNode4.textContent = "";
-            newModal.show($modalError);
-            $modalTickets.classList.add("background-hidden");
-            $modalError.addEventListener("hide.bs.modal", () => {
-                $modalTickets.classList.remove("background-hidden");
-            });
-        } else if (validarEmail($inputEmail) == -1){
-            $textNode1.textContent = "El campo email debe cumplir los siguientes requisitos:";
-            $textNode2.textContent = "- Debe tener @.";
-            $textNode3.textContent = "- Debe contener un dominio correcto.";
-            $textNode4.textContent = "- No debe tener puntos el nombre del correo.";
-            newModal.show($modalError);
-            $modalTickets.classList.add("background-hidden");
-            $modalError.addEventListener("hide.bs.modal", () => {
-                $modalTickets.classList.remove("background-hidden");
-            });
-        } else{
-            if (parseInt($inputCantidad.value) < 0){
-                $textNode1.textContent = "No es posible calcular el valor final en base a una cantidad negativa.";
-                newModal.show($modalError);
-                $modalTickets.classList.add("background-hidden");
-                $modalError.addEventListener("hide.bs.modal", () => {
-                    $modalTickets.classList.remove("background-hidden");
-                });
-            } else{
-                $inputStaticValorFinal.value = "Total a pagar: $";
-                let valorFinalBruto = parseInt($inputCantidad.value) * 200;
-                switch ($optionList.selectedIndex){
-                    case 0:
-                        $inputStaticValorFinal.value += valorFinalBruto;
-                        break;
-                    case 1:
-                        $inputStaticValorFinal.value += valorFinalBruto - (valorFinalBruto * 0.8);
-                        break;
-                    case 2:
-                        $inputStaticValorFinal.value += valorFinalBruto - (valorFinalBruto * 0.5);
-                        break;
-                    case 3:
-                        $inputStaticValorFinal.value += valorFinalBruto - (valorFinalBruto * 0.15);
-                        break;
-                }
-            }
-        }        
-    }
-    if (e.target.matches("#form-btn-borrar")){
-        e.preventDefault();
-        /* Totalmente innecesario esta parte, ya que podría haber accedido por ID a cada uno de los elementos
-           y setear los valores que correspondiera para vaciarlos. Pero quería accederlo haciendo uso de las
-           NodeList, utilizando forEach y entendiendo un poco más del DOM. 
-        */
-        $formTickets.childNodes.forEach((nodeRow) => {
-            if (nodeRow.nodeName == "DIV" && nodeRow.classList.contains("row")){
-                nodeRow.childNodes.forEach((nodeCol) => {
-                    if (nodeCol.nodeName == "DIV"){
-                        if (nodeCol.classList.contains("col-name")){
-                            nodeCol.firstElementChild.value = "";
-                        }
-                        if (nodeCol.classList.contains("col-surname")){
-                            nodeCol.firstElementChild.value = "";
-                        }
-                        if (nodeCol.classList.contains("col")){
-                            switch(nodeCol.firstElementChild.id){
-                                case "form-email":
-                                    nodeCol.firstElementChild.value = "";
-                                    break;
-                                case "valor-final":
-                                    nodeCol.firstElementChild.value = "Total a pagar: $";
-                                    break;
-                            }
-                            switch(nodeCol.lastElementChild.id){
-                                case "form-cantidad":
-                                    nodeCol.lastElementChild.value = 1;
-                                    break;
-                                case "form-categorias":
-                                    nodeCol.lastElementChild.options.selectedIndex = 0;
-                                    break;
-                            }
-                        }
-                    }
-                })
-            }
-        });
-    }
-});
-
-/* Boxes descuento - Funcionalidad como botones */
+/* Referencias a los boxes de descuento y el select del formulario tickets */
 const $contenedorDescuentos = document.querySelector("#categorias-descuento");
 const $selectCategorias = document.querySelector("#form-categorias");
 const $boxEstudiante = document.querySelector("#box-estudiante");
 const $boxTrainee = document.querySelector("#box-trainee");
 const $boxJunior = document.querySelector("#box-junior");
 
-ElegirCategoria($boxEstudiante, 1);
-ElegirCategoria($boxTrainee, 2);
-ElegirCategoria($boxJunior, 3);
+/* Creo un modal que servirá para mostrar encima del modal de tickets (manejo de errores) */
+addCustomPToModalError($modalBodyError);
+const newModal = new bootstrap.Modal("#modal-error", {keyboard: false});
+
+/* Referencia a los parrafos internos del modal Error*/
+const $pText1 = document.querySelector("#text-node1");
+const $pText2 = document.querySelector("#text-node2");
+const $pText3 = document.querySelector("#text-node3");
+const $pText4 = document.querySelector("#text-node4");
+
+$modalTicket.addEventListener("click", (e) => {
+    if (e.target.matches("#form-btn-resumen")){
+        e.preventDefault();
+        if (validarNomApe($inputNombre) == -1 || validarNomApe($inputApellido) == -1){
+            textoValidarNomApe($pText1, $pText2, $pText3, $pText4);
+            mostrarModalError($modalError, $modalTicket, newModal);
+        } else if (validarEmail($inputEmail) == -1){
+            textoValidarEmail($pText1, $pText2, $pText3, $pText4);
+            mostrarModalError($modalError, $modalTicket, newModal);
+        } else{
+            if (parseInt($inputCantidad.value) < 0){
+                textoValidarNegativo($pText1, $pText2, $pText3, $pText4);
+                mostrarModalError($modalError, $modalTicket, newModal);
+            } else{
+                setValorFinalPorCategoria($optionList, $inputStaticValorFinal, $inputCantidad);
+            }
+        }        
+    }
+    if (e.target.matches("#form-btn-borrar")){
+        e.preventDefault();
+        // Limpio todos los inputs y el optionlist por defecto
+        $inputNombre.value = "";
+        $inputApellido.value = "";
+        $inputEmail.value = "";
+        $inputStaticValorFinal.value = "Total a pagar: $";
+        $inputCantidad.value = 1;
+        $optionList.options.selectedIndex = 0;
+    }
+});
+
+/* Boxes descuento - Funcionalidad como botones */
+elegirCategoria($boxEstudiante, 1, $selectCategorias);
+elegirCategoria($boxTrainee, 2, $selectCategorias);
+elegirCategoria($boxJunior, 3, $selectCategorias);
 
 $contenedorDescuentos.addEventListener("click", e => {
     if (e.target.matches("#box-estudiante")){
@@ -137,13 +78,3 @@ $contenedorDescuentos.addEventListener("click", e => {
         $selectCategorias.value = 3;
     }
 });
-
-function ElegirCategoria(emisor, numeroCategoria){
-    if (emisor != null){
-        emisor.addEventListener("click", event => {
-            if (event.target.matches(".text-center")){
-                $selectCategorias.value = numeroCategoria;
-            }
-        });
-    }
-}
---
+++ js/auxFunctions.js
@@ -0,0 +1,79 @@
+const addCustomPToModalError = (modalErrorReference) => {
+    const $textNode1 = createElementCustom("p", [], "", {"id":"text-node1"});
+    const $textNode2 = createElementCustom("p", [], "", {"id":"text-node2"});
+    const $textNode3 = createElementCustom("p", [], "", {"id":"text-node3"});
+    const $textNode4 = createElementCustom("p", [], "", {"id":"text-node4"});
+    modalErrorReference.appendChild($textNode1);
+    modalErrorReference.appendChild($textNode2);
+    modalErrorReference.appendChild($textNode3);
+    modalErrorReference.appendChild($textNode4);
+}
+const createElementCustom = (tipoElemento, clases, contenidoTexto, atributos) => {
+    const newElement = document.createElement(tipoElemento);
+    if (Array.isArray(clases) && clases.length > 0){
+        newElement.classList.add(...clases);
+    }
+    if (contenidoTexto.length > 0 && contenidoTexto != null && contenidoTexto != undefined){
+        newElement.textContent = contenidoTexto;
+    }
+    if (typeof(atributos) == "object"){
+        Object.keys(atributos).forEach((atributo) => {
+            newElement.setAttribute(atributo, atributos[atributo]);
+        })
+    }
+    return newElement;
+}
+const textoValidarNomApe = (textNode1, textNode2, textNode3, textNode4) => {
+    textNode1.textContent = "Los campos nombre y apellido deben tener:";
+    textNode2.textContent = "- Mínimo 3 caracteres y máximo 24.";
+    textNode3.textContent = "- Puede contener acentos y mayúsculas.";
+    textNode4.textContent = "";
+}
+const textoValidarEmail = (textNode1, textNode2, textNode3, textNode4) => {
+    textNode1.textContent = "El campo email debe cumplir los siguientes requisitos:";
+    textNode2.textContent = "- Debe tener @.";
+    textNode3.textContent = "- Debe contener un dominio correcto.";
+    textNode4.textContent = "- No debe tener puntos el nombre del correo.";
+}
+const textoValidarNegativo = (textNode1, textNode2, textNode3, textNode4) => {
+    textNode1.textContent = "No es posible calcular el valor final en base a una cantidad negativa.";
+    textNode2.textContent = "";
+    textNode3.textContent = "";
+    textNode4.textContent = "";
+}
+const elegirCategoria = (emisor, numeroCategoria, referenciaSelectCategorias) => {
+    if (emisor != null){
+        emisor.addEventListener("click", event => {
+            if (event.target.matches(".text-center")){
+                referenciaSelectCategorias.value = numeroCategoria;
+            }
+        });
+    }
+}
+const mostrarModalError = (referenciaModalError, referenciaModalTicket, nuevoModal) => {
+    nuevoModal.show(referenciaModalError);
+    referenciaModalTicket.classList.add("background-hidden");
+    referenciaModalError.addEventListener("hide.bs.modal", () => {
+        referenciaModalTicket.classList.remove("background-hidden");
+    });
+}
+const setValorFinalPorCategoria = (referenciaOptionList, referenciaInputValorFinal, referenciaInputCantidad) => {
+    referenciaInputValorFinal.value = "Total a pagar: $";
+    let valorFinalBruto = parseInt(referenciaInputCantidad.value) * 200;
+    switch (referenciaOptionList.selectedIndex){
+        case 0:
+            referenciaInputValorFinal.value += valorFinalBruto;
+            break;
+        case 1:
+            referenciaInputValorFinal.value += valorFinalBruto - (valorFinalBruto * 0.8);
+            break;
+        case 2:
+            referenciaInputValorFinal.value += valorFinalBruto - (valorFinalBruto * 0.5);
+            break;
+        case 3:
+            referenciaInputValorFinal.value += valorFinalBruto - (valorFinalBruto * 0.15);
+            break;
+    }
+}
+
+export {addCustomPToModalError, createElementCustom, textoValidarNomApe, textoValidarEmail, textoValidarNegativo, elegirCategoria, mostrarModalError, setValorFinalPorCategoria}
