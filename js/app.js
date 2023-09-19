1dc0b28b5282c975f99e53df88044140a9603106
Nico Desktop <diaznicox96@gmail.com>
2023-09-19 12:42:51 -0300

    feat: cleaning js code and refactor

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 import { validarEmail, validarNomApe } from "./validaciones.js";
 import { addCustomPToModalError,
-        textoValidarNomApe,
-        textoValidarEmail,
-        textoValidarNegativo,
+        textoErrorValidarNomApe,
+        textoErrorValidarEmail,
+        textoErrorValidarNegativo,
         elegirCategoria,
         mostrarModalError,
         setValorFinalPorCategoria } from "./auxFunctions.js";
@@ -20,39 +20,30 @@ const $inputNombre = document.querySelector("#form-nombre");
 const $inputApellido = document.querySelector("#form-apellido");
 const $inputEmail = document.querySelector("#form-email");
 
-/* Referencias a los boxes de descuento y el select del formulario tickets */
-const $contenedorDescuentos = document.querySelector("#categorias-descuento");
-const $selectCategorias = document.querySelector("#form-categorias");
-const $boxEstudiante = document.querySelector("#box-estudiante");
-const $boxTrainee = document.querySelector("#box-trainee");
-const $boxJunior = document.querySelector("#box-junior");
-
 /* Creo un modal que servirá para mostrar encima del modal de tickets (manejo de errores) */
 addCustomPToModalError($modalBodyError);
 const newModal = new bootstrap.Modal("#modal-error", { keyboard: false });
 
 /* Referencia a los parrafos internos del modal Error*/
-const $pText1 = document.querySelector("#text-node1");
-const $pText2 = document.querySelector("#text-node2");
-const $pText3 = document.querySelector("#text-node3");
-const $pText4 = document.querySelector("#text-node4");
+const arrayTextNodes = [];
+
+for (let i = 0; i < 4; i++){
+    arrayTextNodes.push(document.querySelector(`#text-node${i+1}`));
+}
 
 $modalTicket.addEventListener("click", (e) => {
     if (e.target.matches("#form-btn-resumen")){
         e.preventDefault();
         if (!validarNomApe($inputNombre) || !validarNomApe($inputApellido)){
             // Si falla al validar nombre o apellido, muestro el modal error con el texto correspondiente
-            textoValidarNomApe($pText1, $pText2, $pText3, $pText4);
-            mostrarModalError($modalError, $modalTicket, newModal);
+            mostrarModalError($modalError, $modalTicket, newModal, textoErrorValidarNomApe, arrayTextNodes);
         } else if (!validarEmail($inputEmail)){
             // Si falla al validar email, muestro el modal error con el texto correspondiente
-            textoValidarEmail($pText1, $pText2, $pText3, $pText4);
-            mostrarModalError($modalError, $modalTicket, newModal);
+            mostrarModalError($modalError, $modalTicket, newModal, textoErrorValidarEmail, arrayTextNodes);
         } else{
             if (Number($inputCantidad.value) < 0){
                 // Si falla al validar cantidad positiva, muestro el modal error con el texto correspondiente
-                textoValidarNegativo($pText1, $pText2, $pText3, $pText4);
-                mostrarModalError($modalError, $modalTicket, newModal);
+                mostrarModalError($modalError, $modalTicket, newModal, textoErrorValidarNegativo, arrayTextNodes);
             } else{
                 // Si pasa las validaciones, realizo el cálculo correspondiente según cada descuento
                 setValorFinalPorCategoria($optionList, $inputStaticValorFinal, $inputCantidad);
@@ -72,9 +63,13 @@ $modalTicket.addEventListener("click", (e) => {
 });
 
 /* Boxes descuento - Funcionalidad como botones */
-elegirCategoria($boxEstudiante, 1, $selectCategorias);
-elegirCategoria($boxTrainee, 2, $selectCategorias);
-elegirCategoria($boxJunior, 3, $selectCategorias);
+/* Referencias a los boxes de descuento y el select del formulario tickets */
+const $contenedorDescuentos = document.querySelector("#categorias-descuento");
+const $selectCategorias = document.querySelector("#form-categorias");
+
+elegirCategoria(document.querySelector("#box-estudiante"), 1, $selectCategorias);
+elegirCategoria(document.querySelector("#box-trainee"), 2, $selectCategorias);
+elegirCategoria(document.querySelector("#box-junior"), 3, $selectCategorias);
 
 $contenedorDescuentos.addEventListener("click", e => {
     if (e.target.matches("#box-estudiante")) $selectCategorias.value = 1;
--- js/auxFunctions.js
+++ js/auxFunctions.js
@@ -1,21 +1,12 @@
 const addCustomPToModalError = (modalErrorReference) => {
-    const $textNode1 = createElementCustom("p", [], "", {"id":"text-node1"});
-    const $textNode2 = createElementCustom("p", [], "", {"id":"text-node2"});
-    const $textNode3 = createElementCustom("p", [], "", {"id":"text-node3"});
-    const $textNode4 = createElementCustom("p", [], "", {"id":"text-node4"});
-    modalErrorReference.appendChild($textNode1);
-    modalErrorReference.appendChild($textNode2);
-    modalErrorReference.appendChild($textNode3);
-    modalErrorReference.appendChild($textNode4);
+    for (let i = 0; i < 4; i++){
+        modalErrorReference.appendChild(createElementCustom("p", [], "", {"id":`text-node${i+1}`}));
+    }
 }
 const createElementCustom = (tipoElemento, clases, contenidoTexto, atributos) => {
     const newElement = document.createElement(tipoElemento);
-    if (Array.isArray(clases) && clases.length > 0){
-        newElement.classList.add(...clases);
-    }
-    if (contenidoTexto.length > 0 && contenidoTexto != null && contenidoTexto != undefined){
-        newElement.textContent = contenidoTexto;
-    }
+    if (Array.isArray(clases) && clases.length > 0) newElement.classList.add(...clases);
+    if (contenidoTexto.length > 0 && contenidoTexto) newElement.textContent = contenidoTexto;
     if (typeof(atributos) == "object"){
         Object.keys(atributos).forEach((atributo) => {
             newElement.setAttribute(atributo, atributos[atributo]);
@@ -23,39 +14,42 @@ const createElementCustom = (tipoElemento, clases, contenidoTexto, atributos) =>
     }
     return newElement;
 }
-const textoValidarNomApe = (textNode1, textNode2, textNode3, textNode4) => {
+/* Funciones auxiliares para el modal de error */
+const textoErrorValidarNomApe = (textNode1, textNode2, textNode3, textNode4) => {
     textNode1.textContent = "Los campos nombre y apellido deben tener:";
     textNode2.textContent = "- Mínimo 3 caracteres y máximo 24.";
     textNode3.textContent = "- Puede contener acentos y mayúsculas.";
     textNode4.textContent = "";
 }
-const textoValidarEmail = (textNode1, textNode2, textNode3, textNode4) => {
+const textoErrorValidarEmail = (textNode1, textNode2, textNode3, textNode4) => {
     textNode1.textContent = "El campo email debe cumplir los siguientes requisitos:";
     textNode2.textContent = "- Debe tener @.";
     textNode3.textContent = "- Debe contener un dominio correcto.";
     textNode4.textContent = "- No debe tener puntos el nombre del correo.";
 }
-const textoValidarNegativo = (textNode1, textNode2, textNode3, textNode4) => {
+const textoErrorValidarNegativo = (textNode1, textNode2, textNode3, textNode4) => {
     textNode1.textContent = "No es posible calcular el valor final en base a una cantidad negativa.";
     textNode2.textContent = "";
     textNode3.textContent = "";
     textNode4.textContent = "";
 }
-const elegirCategoria = (emisor, numeroCategoria, referenciaSelectCategorias) => {
-    if (emisor){
-        emisor.addEventListener("click", event => {
-            if (event.target.matches(".text-center")){
-                referenciaSelectCategorias.value = numeroCategoria;
-            }
-        });
-    }
-}
-const mostrarModalError = (referenciaModalError, referenciaModalTicket, nuevoModal) => {
+const mostrarModalError = (referenciaModalError, referenciaModalTicket, nuevoModal, funcTextError, arrayTextNodes) => {
+    funcTextError(arrayTextNodes[0],arrayTextNodes[1], arrayTextNodes[2], arrayTextNodes[3]);
     nuevoModal.show(referenciaModalError);
     referenciaModalTicket.classList.add("background-hidden");
     referenciaModalError.addEventListener("hide.bs.modal", () => {
         referenciaModalTicket.classList.remove("background-hidden");
     });
+}
+/* ------------------------------------------- */
+
+/* Funciones auxiliares para el manejo del modal de descuentos */
+const elegirCategoria = (emisor, numeroCategoria, referenciaSelectCategorias) => {
+    if (emisor){
+        emisor.addEventListener("click", event => {
+            if (event.target.matches(".text-center")) referenciaSelectCategorias.value = numeroCategoria;
+        });
+    }
 }
 const setValorFinalPorCategoria = (referenciaOptionList, referenciaInputValorFinal, referenciaInputCantidad) => {
     referenciaInputValorFinal.value = "Total a pagar: $";
@@ -75,12 +69,12 @@ const setValorFinalPorCategoria = (referenciaOptionList, referenciaInputValorFin
             break;
     }
 }
-
+/* ----------------------------------------------------------- */
 export { addCustomPToModalError, 
     createElementCustom, 
-    textoValidarNomApe, 
-    textoValidarEmail, 
-    textoValidarNegativo, 
+    textoErrorValidarNomApe, 
+    textoErrorValidarEmail, 
+    textoErrorValidarNegativo, 
     elegirCategoria, 
     mostrarModalError, 
     setValorFinalPorCategoria }
